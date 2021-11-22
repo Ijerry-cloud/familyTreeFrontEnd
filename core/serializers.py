9f78389b3957dec89921d1c046ad839268f68bcf
roronoazor <ugoodumegwu@gmail.com>
2021-11-22 17:38:15 +0100

    feat: updated the core serializers

--- core/serializers.py
+++ core/serializers.py
@@ -4,7 +4,18 @@ from core.models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     
+    gender = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
+    
     class Meta:
         exclude = ["user"]
         model = Profile
+        
+    def get_gender(self, profile):
+        if profile.gender:
+            return profile.gender.capitalize()
+        return ""
+    
+    def get_image(self, profile):
+        pass
         
